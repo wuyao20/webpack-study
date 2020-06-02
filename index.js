83bf9ee7ac630a1c65e80f6658a41287c7632c51
wuyao22 <huyashangding@163.com>
2020-06-02 10:11:07 +0800

    什么是模块打包工具

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+/*
 import Header from "./header.js";
 import Content from "./content.js";
 import Sidebar from "./sidebar.js";
-
+*/
+const Header = require('./header');
+const Content = require('./content');
+const Sidebar = require('./sidebar');
 new Header();
 new Content();
 new Sidebar();
--- readme.md
+++ readme.md
@@ -1,2 +1,14 @@
 # webpack究竟是什么
+
   webpack is a module bundler
+  
+# 什么是模块打包工具
+
+   - concept:
+   
+        modules
+   - api
+   
+        module method 
+        
+        module variables
